493269ec657eefb6ff08934f79726ce2d94bb83d
Peter <p.dyumin@dunice.net>
2020-12-02 12:55:06 +0300

    Load user data on Home page

--- public/js/home.js
+++ public/js/home.js
@@ -1,46 +1,63 @@
 /**
  * Home
  * @param {string} anchor - anchor element
- * @param {*} user - user data
  * @returns {Promise<void>}
  */
-const home = async (anchor = '', user = {}) => {
+const home = async (anchor = '') => {
   const socketsAnchor = 'sockets-anchor';
   const token = localStorage.getItem('token');
 
   $(`#${anchor}`).empty().append(`
     <h1>Home</h1>
-    <div>Hi, ${user.name}!</div>
-    <button
-      id="logout"
-      type="button"
-    >
-      Logout
-    </button>
-    <button
-      id="complete-logout"
-      type="button"
-    >
-      Complete logout
-    </button>
-    <div id="${socketsAnchor}"></div>
+    <div id="content">Loading...</div>
   `);
 
-  $('#logout').on('click', () => {
-    localStorage.removeItem('token');
-    return window.location.reload();
-  });
-
-  $('#complete-logout').on('click', async () => {
-    await $.ajax({
+  try {
+    const { data: { user = {} } = {} } = await $.ajax({
       headers: {
         Authorization: token,
       },
       method: 'GET',
-      url: `${BACKEND_ORIGIN}/api/auth/signout/complete`,
-    })
-    return window.location.reload();
-  });
+      url: `${BACKEND_ORIGIN}/api/user`,
+    });
 
-  sockets(socketsAnchor, token);
+    $(`#content`).empty().append(`
+      <div>Hi, ${user.name}!</div>
+      <button
+        id="logout"
+        type="button"
+      >
+        Logout
+      </button>
+      <button
+        id="complete-logout"
+        type="button"
+      >
+        Complete logout
+      </button>
+      <div id="${socketsAnchor}"></div>
+    `);
+
+    $('#logout').on('click', () => {
+      localStorage.removeItem('token');
+      return window.location.reload();
+    });
+
+    $('#complete-logout').on('click', async () => {
+      await $.ajax({
+        headers: {
+          Authorization: token,
+        },
+        method: 'GET',
+        url: `${BACKEND_ORIGIN}/api/auth/signout/complete`,
+      });
+      return window.location.reload();
+    });
+
+    sockets(socketsAnchor, token);
+  } catch (error) {
+    console.log(error);
+    localStorage.removeItem('token');
+    return window.location.reload();
+  }
 };
